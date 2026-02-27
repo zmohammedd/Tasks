921dce357247e84186a1a4c05a45efb49c2d10e5
zmohammedd <ziadfathy858@gmail.com>
2026-02-27 15:24:28 -0800

    Add files via upload

---
+++ t5.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Magazine
+{
+    public void Show()
+    {
+        Console.WriteLine("Magazine opened.");
+    }
+}
+
+class t5
+{
+    static void Main()
+    {
+        Magazine item = null;
+
+        if (item == null)
+        {
+            Console.WriteLine("empty");
+            item = new Magazine();
+        }
+
+        item.Show();
+    }
+}
