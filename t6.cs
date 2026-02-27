70c6c984d4706cc6a9ecade468c35bdfba677326
zmohammedd <ziadfathy858@gmail.com>
2026-02-27 15:27:36 -0800

    Add files via upload

---
+++ t6.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class t6
+{
+    static void Main()
+    {
+        Console.Write("Enter a temperature: ");
+        int tempValue = int.Parse(Console.ReadLine());
+
+        string message =
+            (tempValue < 10) ? "Just Cold" :
+            (tempValue > 30) ? "Just Hot" :
+            "Just Good";
+
+        Console.WriteLine("Result: " + message);
+    }
+}
