05280b93a4d1a7ea6fcd7b94d9a994033baf4acc
zmohammedd <ziadfathy858@gmail.com>
2026-02-27 15:21:21 -0800

    Add files via upload

---
+++ t4.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Vehicle
+{
+    public string Brand { get; set; }
+}
+
+class t4
+{
+    static void Main()
+    {
+        Vehicle v1 = new Vehicle { Brand = "BMW" };
+        Vehicle v2 = v1;
+
+        v2.Brand = "Audi";
+
+        Console.WriteLine("First object: " + v1.Brand);
+        Console.WriteLine("Second object: " + v2.Brand);
+    }
+}
