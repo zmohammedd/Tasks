73d341bc164a7b6a31cdc2c0ba4d874beead8991
zmohammedd <ziadfathy858@gmail.com>
2026-02-27 15:31:18 -0800

    Add files via upload

---
+++ t7.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class t7
+{
+    static void Main()
+    {
+        Console.Write("Enter number: ");
+        int checkNum = int.Parse(Console.ReadLine());
+
+        string output = (checkNum % 3 == 0 && checkNum % 4 == 0) ? "Yes" : "No";
+        Console.WriteLine(output);
+    }
+}
