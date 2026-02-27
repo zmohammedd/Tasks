4a5de3e1bd789206a6d68b4a93874e44f9f52f67
zmohammedd <ziadfathy858@gmail.com>
2026-02-26 19:51:28 -0800

    Add files via upload

---
+++ t1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+struct valuepoint
+{
+    public int Number { get; set; }
+}
+class refpoint
+{
+    public int Number { get; set; }
+}
+class task
+{
+    static void ChangeData(valuepoint a, refpoint b)
+    {
+        a.Number = 500;   
+        b.Number = 500;   
+    }
+
+    static void Main()
+    {
+        valuepoint first = new valuepoint { Number = 20 };
+        refpoint second = new refpoint { Number = 20 };
+
+        ChangeData(first, second);
+
+        Console.WriteLine("Struct value = " + first.Number);
+        Console.WriteLine("Class value  = " + second.Number);
+    }
+}
