d61855971e801d6093cdbe99f12ef0b71cb66b00
zmohammedd <ziadfathy858@gmail.com>
2026-02-26 19:55:46 -0800

    Add files via upload

---
+++ t2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Learner
+{
+    public string FullName { get; set; }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Learner objA = new Learner { FullName = "Omar" };
+        Learner objB = new Learner { FullName = "Omar" };
+        Learner objC = objA;
+
+        Console.WriteLine("A == B : " + (objA == objB));
+        Console.WriteLine("A == C : " + (objA == objC));
+    }
+}
