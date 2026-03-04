2b039f01fddcefe7f388844d00ce0f3e3fb6994f
zmohammedd <ziadfathy858@gmail.com>
2026-03-04 14:29:02 -0800

    Add files via upload

---
+++ t13.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Account
+{
+    public string Owner { get; set; }
+}
+
+class t13
+{
+    static void ChangeName(Account A)
+    {
+        A.Owner = "UpdatedUser";
+    }
+
+    static void ReplaceAccount(ref Account A)
+    {
+        A = new Account { Owner = "NewAccount" };
+    }
+
+    static void Main()
+    {
+        Account user = new Account { Owner = "OriginalUser" };
+
+        ChangeName(user);
+        Console.WriteLine("After ChangeName: " + user.Owner);
+
+        ReplaceAccount(ref user);
+        Console.WriteLine("After ReplaceAccount: " + user.Owner);
+    }
+}
