f90c4f3d7b0ba56f0de240500556909cd313876c
zmohammedd <ziadfathy858@gmail.com>
2026-03-04 14:47:54 -0800

    Add files via upload

---
+++ t14.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class t14
+{
+    static int Calculate(int a, int b, int c, int d)
+    {
+        int sum_part = a + b;
+        int subtract_part = c + d;
+        return sum_part - subtract_part;
+    }
+
+    static void Main()
+    {
+        Console.Write("Enter first number: ");
+        int n1 = int.Parse(Console.ReadLine());
+        Console.Write("Enter second number: ");
+        int n2 = int.Parse(Console.ReadLine());
+        Console.Write("Enter third number: ");
+        int n3 = int.Parse(Console.ReadLine());
+        Console.Write("Enter fourth number: ");
+        int n4 = int.Parse(Console.ReadLine());
+        int result = Calculate(n1, n2, n3, n4);
+
+        Console.WriteLine("Result = " + result);
+    }
+}
