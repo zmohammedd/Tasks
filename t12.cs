fe97b6ca3feb5eec27eba041f0f2791892fa7b83
zmohammedd <ziadfathy858@gmail.com>
2026-03-04 14:13:04 -0800

    Add files via upload

---
+++ t12.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class t12
+{
+    static void IncreaseCopy(int num)
+    {
+        num += 2;
+    }
+
+    static void IncreaseOriginal(ref int num)
+    {
+        num += 2;
+    }
+
+    static void Main()
+    {
+        int points = 3;
+
+        IncreaseCopy(points);
+        Console.WriteLine("Num: " + points);
+
+        IncreaseOriginal(ref points);
+        Console.WriteLine("After Increase Original: " + points);
+    }
+}
