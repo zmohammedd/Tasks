10f53c7d82f7120512c6bbb2f7260eeecc906215
zmohammedd <ziadfathy858@gmail.com>
2026-03-04 13:55:33 -0800

    Add files via upload

---
+++ t8.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class t8
+{
+    static void Main()
+    {
+        Console.Write("Enter an integer: ");
+        int v = int.Parse(Console.ReadLine());
+        string state = (v < 0) ? "negative" : "positive";
+        Console.WriteLine(state);
+    }
+}
