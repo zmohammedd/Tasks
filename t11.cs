929c009357962db41636d3433c189d972c754cfc
zmohammedd <ziadfathy858@gmail.com>
2026-03-04 14:04:11 -0800

    Add files via upload

---
+++ t11.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class t11
+{
+    static void Main()
+    {
+        Console.Write("Enter a letter: ");
+        string txt = Console.ReadLine().Trim().ToLower();
+        if (txt.Length != 1 || !char.IsLetter(txt[0]))
+        {
+            Console.WriteLine("Invalid input.");
+            return;
+        }
+
+        char letter = txt[0];
+        bool vowel =
+            letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u';
+        Console.WriteLine(vowel ? "vowel" : "consonant");
+    }
+}
