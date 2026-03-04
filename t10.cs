0398094b1e9ceac5efdcf4b7e32dee1ce1dbd0b1
zmohammedd <ziadfathy858@gmail.com>
2026-03-04 14:02:35 -0800

    Add files via upload

---
+++ t10.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class t10
+{
+    static void Main()
+    {
+        Console.Write("Enter a month (1-12): ");
+        int m = int.Parse(Console.ReadLine());
+        int daysCount = 0;
+        bool invalid = true;
+
+        if (m == 2) daysCount = 28;
+        else if (m == 4 || m == 6 || m == 9 || m == 11) daysCount = 30;
+        else if (m >= 1 && m <= 12) daysCount = 31;
+        else invalid = false;
+        if (!invalid) Console.WriteLine("Invalid month.");
+        else Console.WriteLine("Number of days in Month: " + daysCount);
+    }
+}
